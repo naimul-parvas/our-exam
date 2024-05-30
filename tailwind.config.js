ee7fac94066d68296fe06f70580433d0d6f04825
naimul-parvas <rknaimul100@gmail.com>
2024-05-30 01:39:44 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+
+  
+    extend: {
+fontFamily:{
+    abc:"lora"
+    
+
+}
+    },
+  
+  },
+
+  plugins: [],
+
+}
+
+
+
